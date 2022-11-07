3c3998de1caa3b4322a019845e6f97b1e9b8645f
Kirill <109174433+Kirillk1o@users.noreply.github.com>
2022-11-07 19:11:13 +0300

    5. Валидация параметров запуска. [#246865]

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -1,5 +1,6 @@
 package ru.job4j.io;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -9,13 +10,27 @@ import java.util.function.Predicate;
 
 public class Search {
     public static void main(String[] args) throws IOException {
-        Path start = Paths.get("data");
-        search(start, p -> p.toFile().getName().endsWith(".txt")).forEach(System.out::println);
+        validate(args);
+        Path start = Paths.get(args[0]);
+        search(start, p -> p.toFile().getName().endsWith(args[1])).forEach(System.out::println);
     }
 
     public static List<Path> search(Path root, Predicate<Path> condition) throws IOException {
         SearchFiles searcher = new SearchFiles(condition);
         Files.walkFileTree(root, searcher);
         return searcher.getPaths();
+    }
+
+    private static void validate(String[] args) {
+        Path path = Paths.get(args[0]);
+        if (args.length != 2) {
+            throw new IllegalArgumentException("Two arguments are expected");
+        } else if (!Files.exists(path)) {
+            throw new IllegalArgumentException(String.format(
+                    "Not exist %s", path.toFile().getAbsoluteFile()));
+        } else if (!args[1].startsWith(".")) {
+            throw new IllegalArgumentException(String.format(
+                    "Is not a file extencion - %s", args[1]));
+        }
     }
 }
