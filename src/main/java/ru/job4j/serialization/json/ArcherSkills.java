5eb7e83422a37d4703fc8e435f3c90c9037d038a
Kirill <109174433+Kirillk1o@users.noreply.github.com>
2022-11-09 21:47:37 +0300

    4. JAXB. Преобразование XML в POJO. [#315063]

--- src/main/java/ru/job4j/serialization/json/ArcherSkills.java
+++ src/main/java/ru/job4j/serialization/json/ArcherSkills.java
@@ -11,6 +11,7 @@ public class ArcherSkills {
     public ArcherSkills(String atack) {
         this.atack = atack;
     }
+
     public ArcherSkills() {
     }
 
@@ -18,6 +19,6 @@ public class ArcherSkills {
     public String toString() {
         return "ArcherSkills{"
                 + "atack='" + atack + '\''
-        + '}';
+                + '}';
     }
 }
