5ca51a916c6bb37842d2df0839f16f94e6264637
Kirill <109174433+Kirillk1o@users.noreply.github.com>
2022-11-11 19:19:12 +0300

    0.1. Связи между таблицами [#373615 #359935]

--- src/main/java/ru/job4j/sql/table/manyToOnePets.sql
+++ src/main/java/ru/job4j/sql/table/manyToOnePets.sql
@@ -1,17 +1,17 @@
-create table breed(
+create table owner(
 id serial primary key,
 names varchar(255)
 );
 
-create table owners(
+create table dog(
 id serial primary key,
 names varchar(255),
-breed_id int references breed(id)
+owner_id int references owner(id)
 );
 
-insert into breed(names) values ('Мальтипу');
-insert into owners(names, breed_id) VALUES ('Kirill', 1);
+insert into owner(names) VALUES ('Kirill');
+insert into dog(names,owner_id) values ('Мальтипу',1);
 
-select * from owners;
+select * from owner;
 
-select * from breed where id in (select breed_id from owners);
+select * from owner where id in (select dog_id from owner);
