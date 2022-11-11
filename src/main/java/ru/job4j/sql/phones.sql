79241e70faca0e75c08fc0a2b8dcab1eb6c863e2
Kirill <109174433+Kirillk1o@users.noreply.github.com>
2022-11-11 13:43:45 +0300

    0. Что такое база данных. [#6860 #359616]

--- src/main/java/ru/job4j/sql/phones.sql
+++ src/main/java/ru/job4j/sql/phones.sql
@@ -4,10 +4,16 @@ names varchar(200),
 model text,
 amount integer,
 color char (10));
+
 delete from phones;
+
 select * from phones;
+
 insert into phones(name, model, amount, color)
 values('Iphone', 'X', 356, 'Black');
+
 select * from phones;
+
 update phones set amount = 140;
+
 select * from phones;
