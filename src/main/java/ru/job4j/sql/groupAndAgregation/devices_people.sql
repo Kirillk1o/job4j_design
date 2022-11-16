47beee6af3f95642a512871dc436f84f5454b7ef
Kirill <109174433+Kirillk1o@users.noreply.github.com>
2022-11-16 12:06:39 +0300

    2. Группировка и агрегатные функции [#373618 #361180]

--- src/main/java/ru/job4j/sql/groupAndAgregation/devices_people.sql
+++ src/main/java/ru/job4j/sql/groupAndAgregation/devices_people.sql
@@ -42,4 +42,4 @@ from people as p
 join devices_people as dp on dp.people_id = p.id
 join devices as d on dp.device_id = d.id
 group by p.name
-having avg(de.price) > 5000;
+having avg(d.price) > 5000;
