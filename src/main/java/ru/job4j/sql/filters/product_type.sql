ad517dd33a05ddf44fa0b502727050dff2bdb8a7
Kirill <109174433+Kirillk1o@users.noreply.github.com>
2022-11-16 08:55:29 +0300

    3. Фильтры [#1732]

---
+++ src/main/java/ru/job4j/sql/filters/product_type.sql
@@ -0,0 +1,57 @@
+create table type(
+id serial primary key,
+name varchar(255)
+);
+
+create table product(
+id serial primary key,
+name varchar(255),
+expired_date date,
+price float,
+type_id int references type(id)
+);
+
+insert into type (name) values ('Сыр');
+insert into type (name) values ('Мясо');
+insert into type (name) values ('Молоко');
+
+insert into product (name, expired_date, price, type_id) 
+values ('Сыр плавленный','2022-11-01', 400 ,1);
+insert into product (name, expired_date, price, type_id) 
+values ('Сыр моцарела','2022-11-12', 500, 1);
+insert into product (name, expired_date, price, type_id) 
+values ('Курица бройлер', '2022-10-05', 350, 2);
+insert into product (name, expired_date, price, type_id) 
+values ('Мороженное эскимо', '2022-11-07', 150, 2);
+
+select * from product p
+join type t on p.type_id = t.id
+where t.name = 'СЫР';
+
+select * from product p
+where p.name like '%мороженое%';
+
+select * from product p
+join type t on p.type_id = t.id
+where p.expired_date < now();
+
+select p1.name, maxp.mprice from product p1
+join
+(select max(price) as mprice from product) maxp
+on maxp.mprice = p1.price;
+
+select type.name, count(type_id) from product
+join type on product.type_id = type.id
+group by type.name;
+
+select * from product
+join type on product.type_id = type.id
+where type.name = 'СЫР' or type.name = 'МОЛОКО';
+
+select type.name, count(type_id) from product
+join type on product.type_id = type.id
+group by type.name
+having count(type_id) < 10;
+
+select * from product
+join type on product.type_id = type.id;
