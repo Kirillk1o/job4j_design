be3a94c633f9b1f823128678d1c46a83cb740a4d
Kirill <109174433+Kirillk1o@users.noreply.github.com>
2022-11-20 15:00:40 +0300

    Хранилище машин [#1733 #361521]

--- src/main/java/ru/job4j/sql/machineStorage/select.sql
+++ src/main/java/ru/job4j/sql/machineStorage/select.sql
@@ -4,20 +4,17 @@ left join car_bodies cb ON cb.id = c.body_id
 left join car_engines ce ON ce.id = c.engine_id
 left join car_transmissions ct ON ct.id = c.transmission_id
 
-select c.name Авто, cb.name Кузов
+select cb.name Кузов
 from car_bodies cb
 left join cars c on cb.id = c.body_id
-left join car_engines ce on ce.id = c.engine_id
 where c.body_id is null;
 
-select c.name Авто, ce.name Двигатель
+select ce.name Двигатель
 from car_engines ce
 left join cars c on ce.id = c.engine_id
-left join car_bodies cb on cb.id = c.body_id
 where c.engine_id is null;
 
-select c.name Авто, ct.name Трансмиссия
+select ct.name Трансмиссия
 from car_transmissions ct
 left join cars c on ct.id = c.transmission_id
-left join car_engines ce on ce.id = c.engine_id
 where c.transmission_id is null;
