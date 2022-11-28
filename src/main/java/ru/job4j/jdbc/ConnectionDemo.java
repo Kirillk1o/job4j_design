4bc9e8b58455f23a59e0f3c268b4ce6096d0bd28
Kirill <109174433+Kirillk1o@users.noreply.github.com>
2022-11-28 21:33:24 +0300

    0. JDBC [#6863]

--- src/main/java/ru/job4j/jdbc/ConnectionDemo.java
+++ src/main/java/ru/job4j/jdbc/ConnectionDemo.java
@@ -9,19 +9,16 @@ import java.sql.SQLException;
 
 public class ConnectionDemo {
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
+        Config config = new Config("data/app.properties");
+        config.load();
         Class.forName("org.postgresql.Driver");
-        String url = "jdbc:postgresql://localhost:5432/idea_db";
-        String login = "postgres";
-        String password = "86960sdfgq1rt";
+        String url = config.value("hibernate.connection.url");
+        String login = config.value("hibernate.connection.username");
+        String password = config.value("hibernate.connection.password");
         try (Connection connection = DriverManager.getConnection(url, login, password)) {
             DatabaseMetaData metaData = connection.getMetaData();
             System.out.println(metaData.getUserName());
             System.out.println(metaData.getURL());
         }
-        Config config = new Config("data/app.properties");
-        config.load();
-        System.out.println(config.value("hibernate.connection.url"));
-        System.out.println(config.value("hibernate.connection.username"));
-        System.out.println(config.value("hibernate.connection.password"));
     }
 }
